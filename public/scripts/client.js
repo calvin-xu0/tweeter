d492e66c2947519c708d3282a39cb39fe76f52b1
Calvin Xu <calvin.xu.jy@gmail.com>
2021-09-09 00:31:54 +0000

    Fetch tweets with Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,43 +4,13 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
-const renderTweets = function(tweets) {
-  // loops through tweets
+const renderTweets = tweets =>  {
   for (const item of tweets) {
-  // calls createTweetElement for each tweet
     $('#tweets').append($(createTweetElement(item)));
-  // takes return value and appends it to the tweets container
   }
 }
 
-const createTweetElement = (data) => {
+const createTweetElement = data => {
   return `
   <article class="tweet">
   <header>
@@ -69,32 +39,19 @@ const createTweetElement = (data) => {
 `
 }
 
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-}
-
 $(document).ready( function () {
-  renderTweets(data);
 
+  const loadTweets = function() {
+    $.get('/tweets', res => {
+      // console.log(res)
+      renderTweets(res);
+    })
+  }
+  loadTweets();
+  
   $('.new-tweet').find('form').submit( function(evt) {
-    $.post('/tweets/', $(this).serialize(), function() {
-      // console.log('POST request sent')
+    $.post('/tweets', $(this).serialize(), function() {
     })
     evt.preventDefault();
   })
 })
-
-// const $tweet = createTweetElement(tweetData);
-
-// Test / driver code (temporary)
-// console.log($tweet); // to see what it looks like
-// $('#tweets').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
