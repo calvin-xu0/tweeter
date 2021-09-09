1e36ca38cbb2057e18696f393b03df48f92da017
Calvin Xu <calvin.xu.jy@gmail.com>
2021-09-09 01:36:16 +0000

    Prevent cross-site scripting with escaping

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,42 +4,44 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const renderTweets = tweets =>  {
+const renderTweets = tweets => {
   for (const item of tweets) {
     $('#tweets').prepend(createTweetElement(item));
   }
 }
 
 const createTweetElement = data => {
-  return $(`
+  const tweetElement = $(`
   <article class="tweet">
-  <header>
-    <div class="header-left">
-      <img src=${data.user.avatars}>
-      ${data.user.name}
-    </div>
-    <div class="header-right">
-      ${data.user.handle}
-    </div>
-  </header>
-  <p class="tweet-msg">
-    ${data.content.text}
-  </p>
-  <footer>
-    <div class="timestamp">
-      ${timeago.format(data.created_at)}
-    </div>
-    <div class="tweet-actions">
-      <i class="fas fa-flag"></i>
-      <i class="fas fa-retweet"></i>
-      <i class="fas fa-heart"></i>
-    </div>
-  </footer>
-</article>
-`)
+    <header>
+      <div class="header-left">
+        <img src=${data.user.avatars}>
+        ${data.user.name}
+      </div>
+      <div class="header-right">
+        ${data.user.handle}
+      </div>
+    </header>
+    <p class="tweet-msg">
+      
+    </p>
+    <footer>
+      <div class="timestamp">
+        ${timeago.format(data.created_at)}
+      </div>
+      <div class="tweet-actions">
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </div>
+    </footer>
+  </article>
+  `)
+  tweetElement.find('.tweet-msg').text(data.content.text);
+  return tweetElement;
 }
 
-$(document).ready( function () {
+$(document).ready(function() {
 
   const loadTweets = function() {
     $.get('/tweets', res => {
@@ -47,8 +49,8 @@ $(document).ready( function () {
     })
   }
   loadTweets();
-  
-  $('.new-tweet').find('form').submit( function(evt) {
+
+  $('.new-tweet').find('form').submit(function(evt) {
     evt.preventDefault();
 
     const tweetLength = $(this).find('#tweet-text').val().length;
@@ -57,9 +59,10 @@ $(document).ready( function () {
     } else if (tweetLength > 140) {
       window.alert('Tweet too long')
     } else {
-      $.post('/tweets', $(this).serialize());
-      $('#tweets').empty();
-      loadTweets();
+      $.post('/tweets', $(this).serialize()).then(() => {
+        $('#tweets').empty();
+        loadTweets();
+      });
     }
   })
 })
